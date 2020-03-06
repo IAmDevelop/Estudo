5e26ebdcbdb357de1dfe9fc27943c15df53ecaf9
Roberto Santos <57338910+IAmDevelop@users.noreply.github.com>
2020-03-06 19:22:28 -0300

    Delete Default.py

--- Default.py
+++
@@ -1,7 +0,0 @@
-import Conexao
-print("\n Conexao com o MySql - Primeiro Modo \n")
-Conexao.ConectarMySQL2("u355583042_studio", "asenhaestacerta")
-print("\n Conexao com o MySql - Segundo Modo \n")
-Conexao.ConectarMySQL("u355583042_studio", "asenhaestacerta")
-print("\n Conexao com o SQLServer \n")
-Conexao.ConectarSQLServer('sa', 'sahnes10')
